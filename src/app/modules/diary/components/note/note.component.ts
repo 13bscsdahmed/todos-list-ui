8cd1fca5125a9b6e93a61bef7fb2ff334ad2d96f
Danish Ahmed <13bscsdahmed@seecs.edu.pk>
2020-12-18 15:52:35 +0500

    Added simple drag and drop

--- src/app/modules/diary/components/note/note.component.ts
+++ src/app/modules/diary/components/note/note.component.ts
@@ -16,4 +16,45 @@ export class NoteComponent implements OnInit {
   ngOnInit(): void {
   }
   
+  onDragStart(event: any) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+  }
+  
+  onDragOver(event: any) {
+    event.preventDefault();
+  }
+  
+  onDrop(event: any) {
+    const id = event.dataTransfer.getData('text');
+    const draggableElement = document.getElementById(id);
+    let dropzone = event.target;
+    if (!event.target.parentElement.classList.contains('droppable')) {
+      dropzone = event.target.parentElement;
+    }
+    const childToAppend = this.getDragAfterElement(dropzone, event.clientY);
+  
+    
+    if (!childToAppend) {
+      dropzone.appendChild(draggableElement);
+    } else {
+      dropzone.insertBefore(draggableElement, childToAppend);
+    }
+    
+    event.dataTransfer.clearData();
+  }
+  
+  getDragAfterElement(container: any, y: any) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+    
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset, element: child };
+      } else {
+        return closest;
+      }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+  }
+  
 }
